35ebccc254161d775782d05406230bf5f4b7a3d5
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-10-26 12:36:26 +0700

    empty class FormoshlepPlatform added

---
+++ source/formoshlep/platform.d
@@ -0,0 +1,50 @@
+module formoshlep.platform;
+
+import dlangui;
+import dlangui.platforms.common.platform;
+
+class FormoshlepPlatform : Platform
+{
+    override:
+
+    Window createWindow(dstring windowCaption, Window parent, uint flags, uint width, uint height)
+    {
+        return null;
+    }
+
+    void closeWindow(Window w)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    /**
+    * Starts application message loop.
+    *
+    * When returned from this method, application is shutting down.
+    */
+    int enterMessageLoop()
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    bool hasClipboardText(bool mouseBuffer)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    dstring getClipboardText(bool mouseBuffer)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    void setClipboardText(dstring text, bool mouseBuffer)
+    {
+        assert(false, "Isn't implemented");
+    }
+
+    void requestLayout()
+    {
+        //~ if(window !is null)
+            //~ window.requestLayout();
+    }
+}
