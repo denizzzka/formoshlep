5f6fa00ffdd3422c3ca1acd3d9c476c62dfad39c
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-11-08 16:18:35 +0700

    Window declaration moved into main scope

--- example/source/app.d
+++ example/source/app.d
@@ -1,16 +1,13 @@
-import vibe.http.server;
 import dlangui;
 
 mixin APP_ENTRY_POINT;
-
-Window window;
 
 /// entry point for dlangui based application
 extern (C) int UIAppMain(string[] args)
 {
     import formoshlep;
 
-    window = Platform.instance.createWindow("My Window", null);
+    Window window = Platform.instance.createWindow("My Window", null);
 
     window.mainWidget = new VerticalLayout();
     Widget w = new TextWidget("HELLO_WORLD", "Hello, World!"d);
@@ -21,10 +18,10 @@ extern (C) int UIAppMain(string[] args)
     window.mainWidget.addChild(inputBox);
     window.mainWidget.addChild(submitButton);
 
+    import vibe.http.server: HTTPServerSettings;
     auto settings = new HTTPServerSettings;
     settings.port = 8080;
     settings.bindAddresses = ["::1", "127.0.0.1"];
-    settings.sessionStore = new MemorySessionStore;
 
     (cast(FormoshlepPlatform) Platform.instance).httpServerSettings = settings;
 
