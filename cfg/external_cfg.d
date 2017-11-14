03d6538a5c4714731c016029ceb48c598e6f9d87
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-11-14 17:54:53 +0700

    fixes font size

--- cfg/external_cfg.d
+++ cfg/external_cfg.d
@@ -1,1 +1,9 @@
+enum ENABLE_OPENGL = false;
 enum WIDGET_STYLE_CONSOLE = false;
+
+enum BACKEND_GUI = true;
+enum BACKEND_CONSOLE = false;
+enum BACKEND_SDL = false;
+enum BACKEND_X11 = false;
+enum BACKEND_DSFML = false;
+enum BACKEND_WIN32 = false;
